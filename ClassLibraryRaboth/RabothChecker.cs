1827803b8989f583a6b575881c784f0e3e815def
Daimon <118897859+Daimon153@users.noreply.github.com>
2023-06-06 04:04:41 +0500

    Завершение работы

---
+++ ClassLibraryRaboth/RabothChecker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibraryRaboth
+{
+    public class RabothChecker
+    {
+        public static bool LoginAuth(string login, string password)
+        {
+           if(login == "Baltika" && password == "2Wadf")
+            {
+                return true;
+            }
+            if (login == "Pablo" && password == "dsadas")
+            {
+                return true;
+            }
+            if (login == "IvanZolo2004" && password == "2004")
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool WorkerChecker(string FirstName, string LastName,  string SurName, string Num, string INN, string email,
+            string Snils, string Adress, string specialnost, string PasportSeries,string PasportNumber,string Department)
+        {
+           if(FirstName != null && LastName != null && SurName != null && Num != null && INN != null && email != null
+                && Snils != null && PasportSeries != null && PasportNumber != null && Department != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool FioChecker(string FirstName, string LastName, string SurName, string Num, string INN, string email,
+           string Snils, string Adress, string specialnost, string PasportSeries, string PasportNumber, string Department)
+        {
+            if (FirstName != null && LastName != null && SurName != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool TaskChecker(string TaskName, string Opis)
+        {
+            if (TaskName != null && Opis != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ RabothTest/RabothTest.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibraryRaboth;
+namespace RabothTest
+{
+    [TestClass]
+    public class RabothTest
+    {
+        
+        [TestMethod]
+        public void CheckLoginFalse()
+        {
+            //Arrange
+            string login = "Baltos";
+            string password = "ASqw12$$";
+            bool expected = true;
+            //act
+            bool actual = RabothChecker.LoginAuth(login,password);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckLoginTrue()
+        {
+            //Arrange
+            string login = "Baltika";
+            string password = "2Wadf";
+            bool expected = true;
+            //act
+            bool actual = RabothChecker.LoginAuth(login, password);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckWorkerAllFalse()
+        {
+            //Arrange
+            string FirstName = "Жора";
+            string LastName = "Жованна";
+            string SurName = "Жора";
+            string Num = "+79325874589";
+            string INN = "4855898742";
+            string email = "vais232@mail.ru";
+            string Snils = null;
+            string Adress = "Ул.Пушкина 8, 45";
+            string specialnost = "Кондитер";
+            string PasportSeries = "6517";
+            string PasportNumber = "628794";
+            string Department = null;
+
+
+            bool expected = true;
+            //act
+            bool actual = RabothChecker.WorkerChecker(FirstName, LastName, SurName, Num, INN, email, Snils, Adress, specialnost, PasportSeries
+                , PasportNumber, Department);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckWorkerFIOFalse()
+        {
+            //Arrange
+            string FirstName = null;
+            string LastName = null;
+            string SurName = null;
+            string Num = "+79325874589";
+            string INN = "4855898742";
+            string email = "vais232@mail.ru";
+            string Snils = "58428451";
+            string Adress = "Ул.Пушкина 8, 45";
+            string specialnost = "Кондитер";
+            string PasportSeries = "6517";
+            string PasportNumber = "628794";
+            string Department = "Отдел кулинарии";
+
+
+            bool expected = true;
+            //act
+            bool actual = RabothChecker.FioChecker(FirstName, LastName, SurName, Num, INN, email, Snils, Adress, specialnost, PasportSeries
+                , PasportNumber, Department);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckWorkerAllTrue()
+        {
+            //Arrange
+            string FirstName = "Жора";
+            string LastName = "Жованна";
+            string SurName = "Жора";
+            string Num = "+79325874589";
+            string INN = "4855898742";
+            string email = "vais232@mail.ru";
+            string Snils = "58428451";
+            string Adress = "Ул.Пушкина 8, 45";
+            string specialnost = "Кондитер";
+            string PasportSeries = "6517";
+            string PasportNumber = "628794";
+            string Department = "Отдел кулинарии";
+
+
+            bool expected = true;
+            //act
+            bool actual = RabothChecker.WorkerChecker(FirstName, LastName, SurName, Num, INN, email, Snils, Adress, specialnost, PasportSeries
+                , PasportNumber, Department);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TaskCheckerFalse()
+        {
+            //Arrange
+            string TaskName = null;
+            string Opis = "Необходимо разгрузить грузовик с солью";
+           
+
+
+            bool expected = true;
+            //act
+            bool actual = RabothChecker.TaskChecker(TaskName, Opis);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TaskCheckerTrue()
+        {
+            //Arrange
+            string TaskName = "Разгрузка";
+            string Opis = "Необходимо разгрузить грузовик с солью";
+
+
+
+            bool expected = true;
+            //act
+            bool actual = RabothChecker.TaskChecker(TaskName, Opis);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
